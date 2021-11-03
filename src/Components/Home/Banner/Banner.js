0ec601190d2c09cfa2fce182d08dd24a492d9877
AMFahim <amfahim2019@gmail.com>
2021-11-03 21:42:11 +0600

    reat app added

---
+++ src/Components/Home/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import slide1 from '../../../asset/image/slide1'
+
+const Banner = () => {
+  return (
+    <div>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
